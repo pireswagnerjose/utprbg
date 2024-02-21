f7f5ebad41a5f364574875b9fca8061e6993939e
pireswagnerjose@gmail.com <pireswagnerjose@gmail.com>
2024-02-21 00:04:42 -0300

    20-02-2024_2

--- app/Livewire/Main/Prisoner/PrisonerLivewire.php
+++ app/Livewire/Main/Prisoner/PrisonerLivewire.php
@@ -11,11 +11,13 @@ use App\Models\Main\Prisoner;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Layout("layouts.app")]
 #[Title("Pesquisar")]
 class PrisonerLivewire extends Component
 {
+    use WithPagination;
     public $name;
     public $nickname;
     public $cpf;
