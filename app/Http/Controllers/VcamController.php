8c494b12b8c04ed20027df77eca56007e9a84362
Wagner José Pires <pireswagnerjose@gmail.com>
2024-12-16 08:00:40 -0300

    vcam 16-12-2024

--- app/Http/Controllers/VcamController.php
+++ app/Http/Controllers/VcamController.php
@@ -64,7 +64,8 @@ class VcamController extends Controller
             'id',
             'nome',
             'dt. entrada',
-            'dt. saida'
+            'dt. saida',
+            'nr. dias'
         ];
         //Escrever o cabeçalho no arquivo
         fputcsv($csv_open, $title, ';');
@@ -73,13 +74,37 @@ class VcamController extends Controller
         foreach ($prisons_vcam as $id => $prison) {
             // CÁLCULO DOS DIAS -----------------------
             // retorna o a data de entrada do preso na unidade
-            $data_entry = \Carbon\Carbon::parse($prison->entry_date)->format('d/m/Y');
+            
+            $data_entry = \Carbon\Carbon::parse($prison->entry_date);
 
             // retorna a data de saída do preso da unidade
             $exit_date = '';
             if (!empty($prison->exit_date)) {
-               $exit_date = \Carbon\Carbon::parse($prison->exit_date)->format('d/m/Y');
+               $exit_date = \Carbon\Carbon::parse($prison->exit_date);
             }
+
+            // retorna o primeiro dia do mês para cálculo
+            $first_day = \Carbon\Carbon::parse($request->start_date);
+      
+            // retorna o último dia do mês para cálculo = 30 ou 31
+            $last_day = \Carbon\Carbon::parse($request->end_date);
+
+            if (!empty($prison->exit_date)) {
+                if ($first_day->diffInDays($exit_date) < $first_day->diffInDays($last_day)) {
+                    $days = $first_day->diffInDays($exit_date) + 1;
+                } elseif ($data_entry->diffInDays($last_day) < $first_day->diffInDays($last_day)) {
+                    $days = $data_entry->diffInDays($last_day) + 1;
+                } else {
+                    $days = $first_day->diffInDays($last_day) + 1;
+                }
+            } else {
+                if ($data_entry->diffInDays($last_day) < $first_day->diffInDays($last_day)) {
+                    $days = $data_entry->diffInDays($last_day) + 1;
+                } else {
+                    $days = $first_day->diffInDays($last_day) + 1;
+                }
+            }
+
             
             // Criar Array com os dados da linha do excel
             $prisonArr = [
@@ -87,6 +112,7 @@ class VcamController extends Controller
                 'name'      => mb_convert_encoding($prison->prisoner->name, 'ISO-8859-1', 'UTF-8'),
                 'dt_entry'  => $data_entry,
                 'dt_exit'   => $exit_date,
+                'days'      => $days
             ];
             //Escrever o conteúdo no arquivo
             fputcsv($csv_open, $prisonArr, ';');
