d1709385c3d4b9b8526b644153616dced92bc403
Wagner José Pires <pireswagnerjose@gmail.com>
2025-01-09 13:35:51 -0300

    carteirinha 2025-01-09

--- resources/views/livewire/main/identification-card/includes/show-card.blade.php
+++ resources/views/livewire/main/identification-card/includes/show-card.blade.php
@@ -1,71 +1,109 @@
 {{-- carteirinha --}}
 <div class="grid grid-cols-2 justify-stretch gap-2">
-    <div class="border-2 border-zinc-600 dark:border-zinc-600 p-2 rounded-lg">
-        <div class="flex w-full gap-2">
-            <div class="w-[75%]">
-                <div class="flex flex-row w-full">
-                    <div class="w-[20%]">{{-- logo polícia penal --}}
-                        <span class="flex flex-col w-full">
-                            <img class="max-h-48 rounded-lg" src='{{ asset("storage/site/policia_penal_logo.png") }}' alt="{{ $identification_card->visitant->name }}">
-                        </span>
-                    </div>
-                    <div class="w-[80%] text-center">{{-- cabeçalho --}}
-                        <span class="text-xs block">ESTADO DO TOCANTINS</span>
-                        <span class="text-base font-bold block">SECRETARIA DA CIDADANIA E JUSTIÇA</span>
-                        <span class="text-xs font-bold block">SUPERINTENDÊNCIA DE ADMINISTRAÇÃO <br>PENITENCIÁRIO E PRISIONAL</span>
-                        <span class="text-2xl font-bold block">UTPRBG</span>
-                        <span class="text-xs font-bold block">ARAGUAÍNA/TO</span>
-                    </div>
-                </div>
-                <div class="bg-zinc-700 mt-1 rounded-lg">{{-- identificação da carteirinha --}}
-                    <h1 class="text-zinc-100 text-2xl font-bold p-2 text-center">{{ $identification_card->type }}</h1>
-                </div>
+   <div class="border-2 border-zinc-600 dark:border-zinc-600 p-2 rounded-lg">
+      <div class="flex w-full gap-2">
+         <div class="w-[75%]">
+            <div class="flex flex-row w-full">
+               <div class="w-[20%]">{{-- logo polícia penal --}}
+                  <span class="flex flex-col w-full">
+                     <img class="max-h-48 rounded-lg" src='{{ asset("storage/site/policia_penal_logo.png") }}'
+                        alt="{{ $identification_card->visitant->name }}">
+                  </span>
+               </div>
+               <div class="w-[80%] text-center">{{-- cabeçalho --}}
+                  <span class="text-xs block">ESTADO DO TOCANTINS</span>
+                  <span class="text-base font-bold block">SECRETARIA DA CIDADANIA E JUSTIÇA</span>
+                  <span class="text-xs font-bold block">SUPERINTENDÊNCIA DE ADMINISTRAÇÃO <br>PENITENCIÁRIO E
+                     PRISIONAL</span>
+                  <span class="text-2xl font-bold block">UTPRBG</span>
+                  <span class="text-xs font-bold block">ARAGUAÍNA/TO</span>
+               </div>
             </div>
-            <div class="w-[25%]">{{-- foto do perfil --}}
-                <span class="flex flex-col w-full h-full">
-                    <img class="object-cover h-full rounded-lg" src='{{ asset("storage/" . $identification_card->visitant->photo ) }}' alt="{{ $identification_card->visitant->name }}">
-                </span>
+            <div class="bg-zinc-700 mt-1 rounded-lg">{{-- identificação da carteirinha --}}
+               <h1 class="text-zinc-100 text-2xl font-bold p-2 text-center">{{ $identification_card->type }}</h1>
             </div>
-        </div>
-        <div class="mt-2 space-y-2">
-            <span class="text-sm block">VISITANTE: <strong class="text-base">{{ $identification_card->visitant->name }}</strong></span>
-            <span class="text-sm block">REEDUCANDO: <strong class="text-base">{{ $identification_card->prisoner->name }}</strong></span>
-            <span class="text-sm block">PARENTESCO: <strong class="text-base">{{ $identification_card->degree_of_kinship->degree_of_kinship }}</strong></span>
-            <div class="flex justify-between">
-                <span class="text-base">CÓD.: <strong class="text-xl text-red-700">0000{{ $identification_card->id }} </strong></span>
-                <span class="text-base">VALIDADE: <strong class="text-xl text-red-700">{{ \Carbon\Carbon::parse($identification_card->expiration_date)->format('d/m/Y') }}</strong></span>
-            </div>
-        </div>
-    </div>
-    <div class="border-2 border-zinc-600 dark:border-zinc-600 p-2 rounded-lg">
-        <div class="mt-16 w-3/4 mx-auto border-t border-zinc-300 dark:border-zinc-600 text-center text-xs">ASSINATURA DO TITULAR OU RESPONSÁVEL</div>
-        <div>
-            <ul class="text-sm mt-8">
-                <li>Esta carteira é de uso pessoal do titular e intransferível;</li>
-                <li>É obrigatório a apresentação da identidade junto com esta carteira;</li>
-                <li>A perda, furto ou extravio desta carteira deverá ser imediatamente comunicado ä UTPRBG;</li>
-                <li>O uso indevido por terceiros implicará na suspensão do direito de visita;</li>
-                <li>É obrigatório a apresentação desta plastificada.</li>
-            </ul>
-        </div>
-        <div class="mt-16 w-3/4 mx-auto border-t border-zinc-300 dark:border-zinc-600 text-center text-xs">DIRETOR DA UTPRBG</div>
-    </div>
+         </div>
+         <div class="w-[25%]">{{-- foto do perfil --}}
+            <span class="flex flex-col w-full h-full">
+               <img class="object-cover h-full rounded-lg"
+                  src='{{ asset("storage/" . $identification_card->visitant->photo ) }}'
+                  alt="{{ $identification_card->visitant->name }}">
+            </span>
+         </div>
+      </div>
+      <div class="mt-2 space-y-2">
+         <span class="text-sm block">
+            VISITANTE:
+            <strong class="text-base">
+               {{ $identification_card->visitant->name }}
+            </strong>
+         </span>
+         <span class="text-sm block">
+            REEDUCANDO:
+            <strong class="text-base">
+               {{ $identification_card->prisoner->name }}
+            </strong>
+         </span>
+         <div class="flex flex-row gap-4 justify-between">
+            <span class="text-sm block">
+               PARENTESCO:
+               <strong class="text-base">
+                  {{ $identification_card->degree_of_kinship->degree_of_kinship }}
+               </strong>
+            </span>
+            <span class="text-sm block">
+               CPF:
+               <strong class="text-base">
+                  {{ $identification_card->visitant->cpf }}
+               </strong>
+            </span>
+         </div>
+         <div class="flex justify-between">
+            <span class="text-base">
+               CÓD.:
+               <strong class="text-xl text-red-700">
+                  {{ $identification_card->id }}
+               </strong>
+            </span>
+            <span class="text-base">
+               VALIDADE:
+               <strong class="text-xl text-red-700">
+                  {{ \Carbon\Carbon::parse($identification_card->expiration_date)->format('d/m/Y') }}
+               </strong>
+            </span>
+         </div>
+      </div>
+   </div>
+   <div class="border-2 border-zinc-600 dark:border-zinc-600 p-2 rounded-lg">
+      <div>
+         <ul class="text-sm mt-4">
+            <li>Esta carteira é de uso pessoal do titular e intransferível;</li>
+            <li>É obrigatório a apresentação da identidade junto com esta carteira;</li>
+            <li>A perda, furto ou extravio desta carteira deverá ser imediatamente comunicado ä UTPRBG;</li>
+            <li>O uso indevido por terceiros implicará na suspensão do direito de visita;</li>
+            <li>É obrigatório a apresentação desta plastificada.</li>
+         </ul>
+      </div>
+      <div class="mt-36 w-3/4 mx-auto border-t border-zinc-300 dark:border-zinc-600 text-center text-xs">DIRETOR DA
+         UTPRBG</div>
+   </div>
 </div>
 
 {{-- Conteúdo da Página --}}
 <div class="mt-12">
-    <div class="w-full col-span-10">
-        {{-- linha 1 --}}
-        <div class=" mb-5">
-            <x-item-topic>Data do Cadastro</x-item-topic>
-            @empty(!$identification_card->date_of_creation)
-                <x-item-data>{{ \Carbon\Carbon::parse($identification_card->date_of_creation)->format('d/m/Y') }}</x-item-data>
-            @endempty
-        </div>
-        <div>
-            <x-item-topic>Informações Complementares</x-item-topic>
-            <x-item-data>{{ $identification_card->remark }}</x-item-data>
-        </div>
-    </div>
+   <div class="w-full col-span-10">
+      {{-- linha 1 --}}
+      <div class=" mb-5">
+         <x-item-topic>Data do Cadastro</x-item-topic>
+         @empty(!$identification_card->date_of_creation)
+         <x-item-data>{{ \Carbon\Carbon::parse($identification_card->date_of_creation)->format('d/m/Y') }}
+         </x-item-data>
+         @endempty
+      </div>
+      <div>
+         <x-item-topic>Informações Complementares</x-item-topic>
+         <x-item-data>{{ $identification_card->remark }}</x-item-data>
+      </div>
+   </div>
 </div>
 </div>
--- resources/views/reports/identification-card/identification-card-pdf.blade.php
+++ resources/views/reports/identification-card/identification-card-pdf.blade.php
@@ -2,109 +2,123 @@
 <html lang="pt-br">
 
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Atendimentos Internos</title>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <title>Atendimentos Internos</title>
 
-  <style>
-    *,
-    *:after,
-    *:before {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      text-decoration: none;
-      font-family: Arial, Helvetica, sans-serif;
-      font-size: 10px;
-      border: 0;
-    }
+   <style>
+      *,
+      *:after,
+      *:before {
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+         text-decoration: none;
+         font-family: Arial, Helvetica, sans-serif;
+         font-size: 10px;
+         border: 0;
+      }
 
-    body {
-      margin: 0.7cm;
-      font-size: 100%;
-      list-style-type: none;
-    }
-  </style>
+      body {
+         margin: 0.7cm;
+         font-size: 100%;
+         list-style-type: none;
+      }
+   </style>
 </head>
 
 <body>
-  <div style="width: 345px; height: 200px; border: 1px solid #52525b; padding: 8px; border-radius: 8px;">
-    <div style="width: 100%;">
-      <div style="width: 75%; float: left;">
-        <div style="width: 100%; ">
-          {{-- logo polícia penal --}}
-          <div style="width: 20%; float: left;">
-            <span style="width: 100%;">
-              <img style="width: 100%; border-radius: 8px;" src='{{ asset("storage/site/policia_penal_logo.png") }}'
-                alt="{{ $identification_card->visitant->name }}">
+   <div style="width: 345px; height: 200px; border: 1px solid #52525b; padding: 8px; border-radius: 8px;">
+      <div style="width: 100%;">
+         <div style="width: 75%; float: left;">
+            <div style="width: 100%; ">
+               {{-- logo polícia penal --}}
+               <div style="width: 20%; float: left;">
+                  <span style="width: 100%;">
+                     <img style="width: 100%; border-radius: 8px;"
+                        src='{{ asset("storage/site/policia_penal_logo.png") }}'
+                        alt="{{ $identification_card->visitant->name }}">
+                  </span>
+               </div>
+               {{-- cabeçalho --}}
+               <div style="width: 80%; text-align: center; float: right;">
+                  <span style="font-size: 8px; display: block;">ESTADO DO TOCANTINS</span>
+                  <span style="font-size: 9px; font-weight: bold; display: block;">SECRETARIA DA CIDADANIA E
+                     JUSTIÇA</span>
+                  <span style="font-size: 8px; display: block;">SUPERINTENDÊNCIA DE ADMINISTRAÇÃO
+                     <br>PENITENCIÁRIO E PRISIONAL</span>
+                  <span style="font-size: 20px; font-weight: bold; display: block;">UTPRBG</span>
+                  <span style="font-size: 10px; font-weight: bold; display: block;">ARAGUAÍNA/TO</span>
+               </div>
+            </div>
+            <div style="clear: both;"></div>
+            {{-- identificação da carteirinha --}}
+            <div
+               style="background-color: #3f3f46; width: 97%;  margin-top: -12px; margin-bottom: 4px; border-radius: 8px;">
+               <h1 style="color: #f4f4f5; font-size: 20px; font-weight: 700; padding: 2px; text-align: center;">{{
+                  $identification_card->type }}</h1>
+            </div>
+         </div>
+         {{-- foto do perfil --}}
+         <div style="width: 25%; float: right;">
+            <span style="width: 100%; height: 100%;">
+               <img style="width: 100%; height: 55%; border-radius: 8px;"
+                  src='{{ asset("storage/" . $identification_card->visitant->photo ) }}'
+                  alt="{{ $identification_card->visitant->name }}">
             </span>
-          </div>
-          {{-- cabeçalho --}}
-          <div style="width: 80%; text-align: center; float: right;">
-            <span style="font-size: 8px; display: block;">ESTADO DO TOCANTINS</span>
-            <span style="font-size: 9px; font-weight: bold; display: block;">SECRETARIA DA CIDADANIA E JUSTIÇA</span>
-            <span style="font-size: 8px; display: block;">SUPERINTENDÊNCIA DE ADMINISTRAÇÃO
-              <br>PENITENCIÁRIO E PRISIONAL</span>
-            <span style="font-size: 20px; font-weight: bold; display: block;">UTPRBG</span>
-            <span style="font-size: 10px; font-weight: bold; display: block;">ARAGUAÍNA/TO</span>
-          </div>
-        </div>
-        <div style="clear: both;"></div>
-        {{-- identificação da carteirinha --}}
-        <div style="background-color: #3f3f46; width: 97%;  margin-top: -12px; margin-bottom: 4px; border-radius: 8px;">
-          <h1 style="color: #f4f4f5; font-size: 20px; font-weight: 700; padding: 2px; text-align: center;">{{
-            $identification_card->type }}</h1>
-        </div>
+         </div>
       </div>
-      {{-- foto do perfil --}}
-      <div style="width: 25%; float: right;">
-        <span style="width: 100%; height: 100%;">
-          <img style="width: 100%; height: 55%; border-radius: 8px;"
-            src='{{ asset("storage/" . $identification_card->visitant->photo ) }}'
-            alt="{{ $identification_card->visitant->name }}">
-        </span>
-      </div>
-    </div>
-    <div style="clear: both;"></div>
-    <div class="margin-bottom: 8px;">
-      <span style="font-size: 10px; display: block;">VISITANTE: <strong style="font-size: 13px;">{{
-          $identification_card->visitant->name
-          }}</strong></span>
-      <span style="font-size: 10px; margin-top: 5px; display: block;">REEDUCANDO: <strong style="font-size: 13px;">{{
-          $identification_card->prisoner->name
-          }}</strong></span>
-      <span style="font-size: 10px; margin-top: 5px; display: block;">PARENTESCO: <strong style="font-size: 13px;">{{
-          $identification_card->degree_of_kinship->degree_of_kinship }}</strong></span>
-      <div style="margin-top: 10px;">
-        <span style="font-size: 16px;">CÓD.: <strong style="font-size: 20px; color: #c81e1e;">{{
-            $identification_card->id }}
-          </strong></span>
-        <span style="margin-left: 64px; font-size: 16px;">VALIDADE: <strong style="font-size: 20px; color: #c81e1e;">{{
-            \Carbon\Carbon::parse($identification_card->expiration_date)->format('d/m/Y') }}</strong></span>
-      </div>
-    </div>
-  </div>
+      <div style="clear: both;"></div>
+      <div class="margin-bottom: 8px;">
+         <span style="font-size: 10px; display: block;">VISITANTE: <strong style="font-size: 13px;">{{
+               $identification_card->visitant->name
+               }}</strong></span>
+         <span style="font-size: 10px; margin-top: 5px; display: block;">REEDUCANDO: <strong style="font-size: 13px;">{{
+               $identification_card->prisoner->name
+               }}</strong></span>
+         <div style="font-size: 10px; margin-top: 5px; display: block;">
+            <span>
+               PARENTESCO:
+               <strong style="font-size: 13px;">
+                  {{ $identification_card->degree_of_kinship->degree_of_kinship }}
+               </strong>
+            </span>
 
-  <!-- lado 2 -->
-  <div
-    style="width: 345px; height: 200px; margin-top: 36px; float: left; border: 1px solid #52525b; padding: 8px; border-radius: 8px;">
-    <div style="margin-top: 32px; width: 75%; margin-left: auto; margin-right: auto; border-top: 1px solid #52525b;
-        text-align: center; font-size: 10px;">
-      ASSINATURA DO TITULAR OU RESPONSÁVEL
-    </div>
-    <div>
-      <ul style="font-size: 12px; line-height: 16px; margin-top: 12px; margin-left: 8px;">
-        <li>Esta carteira é de uso pessoal do titular e intransferível;</li>
-        <li>É obrigatório a apresentação da identidade junto com esta carteira;</li>
-        <li>A perda, furto ou extravio desta carteira deverá ser imediatamente comunicado ä UTPRBG;</li>
-        <li>O uso indevido por terceiros implicará na suspensão do direito de visita;</li>
-        <li>É obrigatório a apresentação desta plastificada.</li>
-      </ul>
-    </div>
-    <div
-      style="margin-top: 36px; width: 75%; margin-left: auto; margin-right: auto; border-top: 1px solid #52525b; text-align: center; font-size: 10px;">
-      DIRETOR DA UTPRBG
-    </div>
-  </div>
+            <span style="float: right;">
+               CPF:
+               <strong style="font-size: 13px;">
+                  {{ $identification_card->visitant->cpf }}
+               </strong>
+            </span>
+         </div>
+
+         <div style="margin-top: 8px;">
+            <span style="font-size: 16px;">CÓD.: <strong style="font-size: 20px; color: #c81e1e;">{{
+                  $identification_card->id }}
+               </strong></span>
+            <span style="margin-left: 64px; font-size: 16px;">VALIDADE: <strong
+                  style="font-size: 20px; color: #c81e1e;">{{
+                  \Carbon\Carbon::parse($identification_card->expiration_date)->format('d/m/Y') }}</strong></span>
+         </div>
+      </div>
+   </div>
+
+   <!-- lado 2 -->
+   <div
+      style="width: 345px; height: 200px; margin-top: 36px; float: left; border: 1px solid #52525b; padding: 8px; border-radius: 8px;">
+      <div>
+         <ul style="font-size: 12px; line-height: 16px; margin-top: 12px; margin-left: 8px;">
+            <li>Esta carteira é de uso pessoal do titular e intransferível;</li>
+            <li>É obrigatório a apresentação da identidade junto com esta carteira;</li>
+            <li>A perda, furto ou extravio desta carteira deverá ser imediatamente comunicado ä UTPRBG;</li>
+            <li>O uso indevido por terceiros implicará na suspensão do direito de visita;</li>
+            <li>É obrigatório a apresentação desta plastificada.</li>
+         </ul>
+      </div>
+      <div
+         style="margin-top: 64px; width: 75%; margin-left: auto; margin-right: auto; border-top: 1px solid #52525b; text-align: center; font-size: 10px;">
+         DIRETOR DA UTPRBG
+      </div>
+   </div>
 </body>
 
 </html>
