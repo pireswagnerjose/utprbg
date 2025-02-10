d34a9501835633d0cfa9a5c27bbcfe8773d4c052
Wagner José Pires <pireswagnerjose@gmail.com>
2025-02-10 07:25:52 -0300

    prisoner_create 2025-02-10

--- resources/views/livewire/pages/prisoner/prisoner-livewire.blade.php
+++ resources/views/livewire/pages/prisoner/prisoner-livewire.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- add new --}}
     <div class="flex items-center">
-        @can('show_prisoners')
+        @can('create_prisoner')
         <div>
             <button wire:click="modal" type="button"
                 class="mb-4 text-white bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
