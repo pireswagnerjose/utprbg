ce9aa7b2b34aace79b383aa87fff400aab33ffbc
Wagner José Pires <pireswagnerjose@gmail.com>
2025-02-13 07:34:26 -0300

    delete document visitant 2025-02-13_1

--- resources/views/livewire/main/visitant/visitant-document-livewire.blade.php
+++ resources/views/livewire/main/visitant/visitant-document-livewire.blade.php
@@ -8,33 +8,43 @@
     <div class="grid grid-cols-2 p-2 rounded-lg gap-6">
       @foreach ($visitant_documents as $visitant_document)
       <div class="border-b dark:border-zinc-700 justify-between mx-6">
+
         <div class="text-sm uppercase font-medium">
+
           <a title='{{ $visitant_document->title }}' href='{{ asset("storage/$visitant_document->document") }}'
             rel='shadowbox[galeria]'>
             <dd class="font-semibold text-blue-700 dark:text-blue-500 hover:underline">{{
               $visitant_document->title }}</dd>
           </a>
+
           <div class="text-xs uppercase">
             <dd class=" text-zinc-600 dark:text-zinc-400">{{ $visitant_document->remark }}</dd>
           </div>
+
         </div>
+
         <div class="flex justify-end">
+
           @can('update_document_visitant')
           <button wire:click="modalVisitantDocumentEdit({{ $visitant_document->id }})">
             <span class="text-green-600 text-xs mr-4">[ Editar ] </span>
           </button>
           @endcan
+
           @can('delete_document_visitant')
           <button wire:click="modalVisitantDocumentDelete({{ $visitant_document->id }})">
             <span class="text-red-600 text-xs">[ excluir ]</span>
           </button>
+          @endcan
+
         </div>
-        @endcan
       </div>
       @endforeach
     </div>
   </div>
+
   @include("livewire.main.visitant.includes.document-modal-create")
   @include("livewire.main.visitant.includes.document-modal-update")
   @include("livewire.main.visitant.includes.document-modal-delete")
+
 </div>
