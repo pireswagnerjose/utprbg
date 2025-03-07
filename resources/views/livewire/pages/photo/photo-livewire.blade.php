67d39b3835cef708fe99cd89bc9ed5e9109499d5
Wagner José Pires <pireswagnerjose@gmail.com>
2025-03-07 07:28:21 -0300

    photos 2025-03-07

--- resources/views/livewire/pages/photo/photo-livewire.blade.php
+++ resources/views/livewire/pages/photo/photo-livewire.blade.php
@@ -5,14 +5,14 @@
     </div>
     {{-- add new --}}
     <div class="flex items-center border-b border-blue-400 dark:border-blue-600">
-        {{-- @can('create_photo') --}}
-        <div>
-            <button wire:click="modal" type="button"
-                class="p-2.5 mb-4 bg-blue-600 dark:bg-blue-500/50 hover:opacity-50 transition duration-500 rounded-full">
-                <x-lucide-plus class="w-4 h-4 text-zinc-100 dark:text-zinc-200"></x-lucide-plus>
-            </button>
-        </div>
-        {{-- @endcan --}}
+        @can('create_photo')
+            <div>
+                <button wire:click="modal" type="button"
+                    class="p-2.5 mb-4 bg-blue-600 dark:bg-blue-500/50 hover:opacity-50 transition duration-500 rounded-full">
+                    <x-lucide-plus class="w-4 h-4 text-zinc-100 dark:text-zinc-200"></x-lucide-plus>
+                </button>
+            </div>
+        @endcan
     </div>
     @include('livewire.pages.photo.includes.card')
     <form wire:submit="save">
