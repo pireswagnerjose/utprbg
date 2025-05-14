e354745836a3f0990879a7a5e5129672908bdacc
Wagner José Pires <pireswagnerjose@gmail.com>
2025-05-14 10:01:47 -0300

    update article

--- resources/views/components/report-layout.blade.php
+++ resources/views/components/report-layout.blade.php
@@ -1,12 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Prisoner Report</title>
 
     <style>
-        *, 
+        *,
         *:after,
         *:before {
             margin: 0;
@@ -14,27 +14,33 @@
             box-sizing: border-box;
             text-decoration: none;
             font-family: Arial, Helvetica, sans-serif;
-            border:0;
+            border: 0;
         }
-        body{
-            margin:1cm;
+
+        body {
+            margin: 1cm;
             font-size: 100%;
             list-style-type: none;
+            text-transform: uppercase;
         }
-        header{
+
+        header {
             width: 100%;
             padding-bottom: 2px;
         }
-        header img{
+
+        header img {
             width: 96%;
             align-items: center;
         }
-        main{
+
+        main {
             margin-top: 2px;
             padding-top: 2px;
             border-top: 1px solid #ccc;
         }
-        .title{
+
+        .title {
             font-size: 12px;
             font-weight: bold;
             background-color: #ccc;
@@ -42,61 +48,75 @@
             color: black;
             clear: both;
         }
-        .line{
+
+        .line {
             width: 100%;
             clear: both;
         }
-        .colum{
+
+        .colum {
             float: left;
             margin-bottom: 5px;
         }
-        .item_span{
+
+        .item_span {
             letter-spacing: -0.025em;
             color: #666;
             font-size: 0.6rem;
             line-height: 1;
             margin: 0;
         }
-        .item_p{
-            font-size: 0.75rem/* 12px */;
-            line-height: 1rem/* 16px */;
+
+        .item_p {
+            font-size: 0.75rem
+                /* 12px */
+            ;
+            line-height: 1rem
+                /* 16px */
+            ;
             letter-spacing: -0.025em;
             line-height: 1;
             margin: 0;
             text-align: justify;
         }
-        .box{
+
+        .box {
             width: 100%;
             padding-top: 5px;
         }
 
         /* PRISONER */
-        .prisoner{
+        .prisoner {
             width: 100%;
             padding-top: 5px;
         }
-        .prisoner_img{
+
+        .prisoner_img {
             width: 23%;
             border-radius: 0.5rem;
             border-radius: 5px;
             position: absolute;
             z-index: 50;
-            margin:0;
+            margin: 0;
         }
-        .prisoner_img img{
+
+        .prisoner_img img {
             width: 100%;
             height: 220px;
             border-radius: 5px;
         }
-        .prisoner_data{
+
+        .prisoner_data {
             width: 73%;
             margin-left: 27%;
         }
+
         tr:nth-child(even) {
             background: #CCC
         }
     </style>
 </head>
+
 <body>
     {{-- header --}}
     <header>
@@ -107,4 +127,5 @@
         {{ $slot }}
     </main>
 </body>
+
 </html>
--- resources/views/reports/prisoner/includes/process.blade.php
+++ resources/views/reports/prisoner/includes/process.blade.php
@@ -1,63 +1,79 @@
 {{-- PROCESSOS --}}
 @if (isset($prisoner->processes) && $prisoner->processes->count() > 0)
-<div class="pb-2">
-    {{-- Título --}}
-    <h1 class="title">PROCESSOS</h1>
-    {{-- Conteúdo --}}
-    <section>
-        @foreach ($prisoner->processes as $process)
-        <div class="line">
-            <div class="colum" style="width: 25%;">
-                <span class="item_span">Data da Prisão no Processo: </span>
-                <p class="item_p">{{ \Carbon\Carbon::parse($process->date_arrest)->format('d/m/Y') }}</p>
-            </div>
-            <div class="colum" style="width: 25%;">
-                <span class="item_span">Data de Alvará no Processo: </span>
-                @empty(!$process->date_exit)
-                <p class="item_p">{{ \Carbon\Carbon::parse($process->date_exit)->format('d/m/Y') }}</p>
-                @endempty
-            </div>
-            <div class="colum" style="width: 50%;">
-                <span class="item_span">Regime da Prisão: </span>
-                <p class="item_p">{{ $process->process_regime->process_regime }}</p>
-            </div>
-        </div>
+    <div class="pb-2">
+        {{-- Título --}}
+        <h1 class="title">PROCESSOS</h1>
+        {{-- Conteúdo --}}
+        <section style="border-bottom: 2px solid #666; padding-bottom: 4px; margin-bottom: 12px;">
+            @foreach ($prisoner->processes as $process)
+                <div class="line">
+                    <div class="colum" style="width: 25%;">
+                        <span class="item_span">Data da Prisão no Processo: </span>
+                        <p class="item_p">{{ \Carbon\Carbon::parse($process->date_arrest)->format('d/m/Y') }}</p>
+                    </div>
+                    <div class="colum" style="width: 25%;">
+                        <span class="item_span">Data de Alvará no Processo: </span>
+                        @empty(!$process->date_exit)
+                            <p class="item_p">{{ \Carbon\Carbon::parse($process->date_exit)->format('d/m/Y') }}</p>
+                        @endempty
+                    </div>
+                    <div class="colum" style="width: 50%;">
+                        <span class="item_span">Regime da Prisão: </span>
+                        <p class="item_p">{{ $process->process_regime->process_regime }}</p>
+                    </div>
+                </div>
 
-        <div class="line">
-            <div class="colum" style="width: 34%;">
-                <span class="item_span">Origem do Processo: </span>
-                <p class="item_p">{{ $process->origin_process->origin_process }}</p>
-            </div>
-            <div class="colum" style="width: 33%;">
-                <span class="item_span">Comarca de Origem: </span>
-                <p class="item_p">{{ $process->judicial_district_origin }}</p>
-            </div>
-            <div class="colum" style="width: 33%;">
-                <span class="item_span">Número do EPROC: </span>
-                <p class="item_p">{{ $process->eproc }}</p>
-            </div>
-        </div>
+                <div class="line">
+                    <div class="colum" style="width: 34%;">
+                        <span class="item_span">Origem do Processo: </span>
+                        <p class="item_p">{{ $process->origin_process->origin_process }}</p>
+                    </div>
+                    <div class="colum" style="width: 33%;">
+                        <span class="item_span">Comarca de Origem: </span>
+                        <p class="item_p">{{ $process->judicial_district_origin }}</p>
+                    </div>
+                    <div class="colum" style="width: 33%;">
+                        <span class="item_span">Número do EPROC: </span>
+                        <p class="item_p">{{ $process->eproc }}</p>
+                    </div>
+                </div>
 
-        <div class="line">
-            <div class="colum" style="width: 34%;">
-                <span class="item_span">Número do SEEU: </span>
-                <p class="item_p">{{ $process->seeu }}</p>
-            </div>
-            <div class="colum" style="width: 33%;">
-                <span class="item_span">Número do PJE: </span>
-                <p class="item_p">{{ $process->pje }}</p>
-            </div>
-            <div class="colum" style="width: 33%;">
-                <span class="item_span">Número do APF: </span>
-                <p class="item_p">{{ $process->apf }}</p>
-            </div>
-        </div>
+                <div class="line">
+                    <div class="colum" style="width: 34%;">
+                        <span class="item_span">Número do SEEU: </span>
+                        <p class="item_p">{{ $process->seeu }}</p>
+                    </div>
+                    <div class="colum" style="width: 33%;">
+                        <span class="item_span">Número do PJE: </span>
+                        <p class="item_p">{{ $process->pje }}</p>
+                    </div>
+                    <div class="colum" style="width: 33%;">
+                        <span class="item_span">Número do APF: </span>
+                        <p class="item_p">{{ $process->apf }}</p>
+                    </div>
+                </div>
 
-        <div class="line">
-            <span class="item_span">Observações:</span>
-            <p class="item_p">{{ $process->remark }}</p>
-        </div>
-        @endforeach
-    </section>
-</div>
+                <div>
+                    @foreach ($process->penal_type_processes as $penal_type_process)
+                        <div style="border-bottom: 1px solid #CCC; padding-bottom: 4px;">
+                            <div style="clear: both"></div>
+                            <span style="margin-top: 4px; color: #666; font-size: 0.6rem;">Artigo:</span>
+                            <p class="item_p" style="line-height: 130%;">
+                                - {{ $penal_type_process->penal_type->law }}
+                                {{ $penal_type_process->penal_type->article }}
+                                {{ $penal_type_process->penal_type->paragraph }}
+                                {{ $penal_type_process->penal_type->item }} <span class="font-light text-xs">
+                                    - {{ $penal_type_process->penal_type->description }}</span>
+                            </p>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="line">
+                    <span class="item_span">Observações:</span>
+                    <p class="item_p">{{ $process->remark }}</p>
+                </div>
+            @endforeach
+        </section>
+    </div>
 @endif
